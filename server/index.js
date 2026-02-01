2f724b3d2cd2a743693e94b164636f1a1d10802e
naoi-reii <angchristian95@gmail.com>
2026-02-01 21:29:18 +0800

    fix pdf extract fails when adding a photo

--- server/index.js
+++ server/index.js
@@ -9,8 +9,8 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({ limit: '10mb' }));
+app.use(bodyParser.urlencoded({ extended: true, limit: '10mb' }));
 
 // Basic Route
 app.get('/', (req, res) => {
